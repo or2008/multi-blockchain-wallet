f310c1e74b5033dbf84a20a6818b8a0bb195f1ea
Or Bachar <or@localcoin.com>
2018-11-13 15:55:42 +0200

    add new methods

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,25 @@ import { IWallet } from './common/wallet';
 import { getPluginByType } from './plugins';
 import * as bip39 from 'bip39';
 
-export function generate(type = 'eos'): IWallet {
+// const DEFAULT_TYPES = ['bitcoin', 'ethereum', 'eos', 'tron'];
+
+export function generateMnemonic(strength?, rng?, wordlist?) {
+    return bip39.generateMnemonic(strength, rng, wordlist);
+}
+
+export function generate(type = 'ethereum'): IWallet {
     return getPluginByType(type).generateWallet();
 }
 
-export function createWalletByMnemonic(type, mnemonic): IWallet {
+export function generateMulti(types): IWallet[] {
+    const mnemonic = generateMnemonic();
+    return types.map(type => createWalletByMnemonic(type, mnemonic));
+}
+
+export function createWalletByMnemonic(type: string, mnemonic: string): IWallet {
     return getPluginByType(type).createWalletByMnemonic(mnemonic)
 }
 
-export function generateMulti(types = ['bitcoin', 'ethereum', 'eos', 'tron']): IWallet[] {
-    const mnemonic = bip39.generateMnemonic();
+export function createWalletByMnemonicMulti(types, mnemonic): IWallet[] {
     return types.map(type => createWalletByMnemonic(type, mnemonic));
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,10 +1,19 @@
 import * as assert from 'assert';
-import { generateMulti } from "../src";
+import { generateMulti, generateMnemonic, createWalletByMnemonicMulti } from "../src";
+
+const TYPES = ['bitcoin', 'ethereum', 'eos', 'tron'];
 
 describe('Bancor Module', () => {
     it('should generate multi blockchain wallets by the same mnemonic', () => {
+        const wallets = generateMulti(TYPES);
+        wallets.forEach((wallet) => {
+            assert(wallet.keyPair.privateKey)
+        })
+    });
 
-        const wallets = generateMulti();
+    it('should create multi blockchain wallets by provided mnemonic', () => {
+        const mnemonic = generateMnemonic();
+        const wallets = createWalletByMnemonicMulti(TYPES, mnemonic);
         wallets.forEach((wallet) => {
             assert(wallet.keyPair.privateKey)
         })
