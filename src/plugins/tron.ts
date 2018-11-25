4a64f6d5448452775da41d35a33d2b04bfaf9788
Or Bachar <or@localcoin.com>
2018-11-25 13:30:59 +0200

    fix webpack import

--- src/plugins/tron.ts
+++ src/plugins/tron.ts
@@ -1,7 +1,7 @@
 import * as bip39 from 'bip39';
 import * as bitcoin from 'bitcoinjs-lib';
 import { IWallet } from '../common/wallet';
-import * as TronWeb from 'tronweb';
+import { address } from 'tronweb';
 import { IPlugin } from '../common/plugin';
 
 export const plugin: IPlugin = {
@@ -12,7 +12,7 @@ export const plugin: IPlugin = {
         const derived = master.derivePath("m/44'/195'/0'/0/0");
 
         const privateKey = derived.privateKey.toString('hex');
-        const publicKey = TronWeb.address.fromPrivateKey(privateKey); // TODO - we use a big library just for this line, we should remove the lib
+        const publicKey = address.fromPrivateKey(privateKey); // TODO - we use a big library just for this line, we should remove the lib
 
         return {
             type: 'tron',
