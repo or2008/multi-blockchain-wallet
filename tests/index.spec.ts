206cb0f8faaa23e598a0ea5d8621583824bf95df
Or Bachar <or@localcoin.com>
2018-12-04 16:14:14 +0200

    update eos sign transaction test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -46,7 +46,7 @@ describe('Bancor Module', () => {
             transaction: new Uint8Array([0, 16, 32, 128, 255])
         };
         const signedTransaction = await signTransaction('eos', transaciton, privateKey);
-        assert(signedTransaction[0].includes('SIG'));
+        assert(signedTransaction.signatures[0].includes('SIG'));
     });
 
     it('should sign a sample EOS transaciton as Object', async () => {
@@ -58,7 +58,7 @@ describe('Bancor Module', () => {
             transaction: { 0: 197, 1: 121, 2: 6, 3: 92, 4: 113, 5: 84, 6: 100, 7: 84, 8: 144, 9: 203, 10: 0, 11: 0, 12: 0, 13: 0, 14: 1, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 128, 22: 117, 23: 0, 24: 0, 25: 0, 26: 87, 27: 45, 28: 60, 29: 205, 30: 205, 31: 1, 32: 64, 33: 194, 34: 40, 35: 67, 36: 16, 37: 50, 38: 238, 39: 60, 40: 0, 41: 0, 42: 0, 43: 0, 44: 168, 45: 237, 46: 50, 47: 50, 48: 33, 49: 64, 50: 194, 51: 40, 52: 67, 53: 16, 54: 50, 55: 238, 56: 60, 57: 64, 58: 200, 59: 40, 60: 162, 61: 148, 62: 32, 63: 238, 64: 60, 65: 0, 66: 225, 67: 245, 68: 5, 69: 0, 70: 0, 71: 0, 72: 0, 73: 8, 74: 73, 75: 81, 76: 0, 77: 0, 78: 0, 79: 0, 80: 0, 81: 0, 82: 0 }
         };
         const signedTransaction = await signTransaction('eos', transaciton, privateKey);
-        assert(signedTransaction[0].includes('SIG'));
+        assert(signedTransaction.signatures[0].includes('SIG'));
     });
 
     it('should sign a sample Ethereum transaciton', () => {
